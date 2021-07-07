773542d223caa0ba737ef7bc4c10cc18729461f8
Patrick Tapping <mail@patricktapping.com>
2021-07-07 14:10:17 +0930

    Delete test files

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from thorlabs_apt_device import APTDevice, BSC201_DRV250, find_device, list_devices, EndPoint, protocol
-from serial.tools import list_ports
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-stage = BSC201_DRV250()
