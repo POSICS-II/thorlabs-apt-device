03fa7907a24ff8899ac8700622b486c6b0de3ba6
Patrick Tapping <mail@patricktapping.com>
2022-02-08 14:00:20 +1030

    Restructure to implement BSC loop params

--- thorlabs_apt_device/devices/bsc.py
+++ thorlabs_apt_device/devices/bsc.py
@@ -13,29 +13,133 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ["BSC201", "BSC201_DRV250"]
+__all__ = ["BSC", "BSC201", "BSC201_DRV250"]
 
 from .. import protocol as apt
 from .aptdevice_motor import APTDevice_BayUnit
 from ..enums import EndPoint
 
-
-class BSC201(APTDevice_BayUnit):
+class BSC(APTDevice_BayUnit):
     """
-    A class for ThorLabs APT device model BSC201.
+    A class for the BSC10x and BSC20x series of Thorlabs APT controllers.
 
-    It is based off :class:`BBD_BSC` configured with a single channel and the BSC the serial number
-    preset in the initialisation method.
+    It is based off
+    :class:`~thorlabs_apt_device.devices.aptdevice_motor.APTDevice_BayUnit` with the
+    addition of the control loop parameters used for encoded stages.
 
-    Note that it seems the BSC201 will send automatic status updates, but does not accept the 
-    corresponding acknowledgement message, and so will stop responding after ~5 seconds if they are
-    enabled. As a workaround, the status of the device will be polled.
+    Note it seems that at least some of the BSC series will send automatic status updates, but does
+    not accept the corresponding acknowledgement message, and so will stop responding after ~5
+    seconds if they are enabled. As a workaround, the status of the device will be polled.
 
     Additionally, it seems that the initial movement and homing velocities can be effectively zero,
     making it seem like the device is not working, though it is actually just moving extremely
-    slowly.
-    It's a good idea to ensure sensible values are set for :meth:`set_velocity_params`, :meth:`set_jog_params`
-    and :meth:`set_home_params` during initialisation.
+    slowly. It's a good idea to ensure sensible values are set for
+    :meth:`~thorlabs_apt_device.devices.aptdevice_motor.APTDevice_Motor.set_velocity_params`,
+    :meth:`~thorlabs_apt_device.devices.aptdevice_motor.APTDevice_Motor.set_jog_params` and
+    :meth:`~thorlabs_apt_device.devices.aptdevice_motor.APTDevice_Motor.set_home_params` during
+    initialisation.
+
+    :param serial_port: Serial port device the device is connected to.
+    :param vid: Numerical USB vendor ID to match.
+    :param pid: Numerical USB product ID to match.
+    :param manufacturer: Regular expression to match to a device manufacturer string.
+    :param product: Regular expression to match to a device product string.
+    :param serial_number: Regular expression to match to a device serial number.
+    :param x: Number of channels the device controls (the x in BSC20x).
+    :param home: Perform a homing operation on initialisation.
+    :param invert_direction_logic: Invert the meaning of "forward" and "reverse" directions.
+    :param swap_limit_switches: Swap "forward" and "reverse" limit switch values.
+    """
+    def __init__(self, serial_port=None, vid=None, pid=None, manufacturer=None, product=None, serial_number=None, location=None, home=True, x=1, invert_direction_logic=False, swap_limit_switches=True):
+
+        super().__init__(serial_port=serial_port, vid=vid, pid=pid, manufacturer=manufacturer, product=product, serial_number=serial_number, location=location, home=home, x=x, invert_direction_logic=invert_direction_logic, swap_limit_switches=swap_limit_switches, status_updates="polled")
+
+        self.loopparams_ = [[{
+            "loop_mode": 0,
+            "prop": 0,
+            "int": 0,
+            "diff": 0,
+            "pid_clip": 0,
+            "pid_tol": 0,
+            "encoder_const": 0,
+            # Update message fields
+            "msg" : "",
+            "msgid" : 0,
+            "source" : 0,
+            "dest" : 0,
+            "chan_ident" : 0,
+        } for _ in self.channels] for _ in self.bays]
+        """
+        Parameters for the closed-loop position control for encoded stages.
+
+        Fields are ``"loop_mode"``, ``"prop"``, ``"int"``, ``"diff"``, ``"pid_clip"``, ``"pid_tol"``, and ``"encoder_const"``.
+        """
+        # Request current control loop parameters
+        for bay in self.bays:
+            for channel in self.channels:
+                self._loop.call_soon_threadsafe(self._write, apt.mot_req_kcubekstloopparams(source=EndPoint.HOST, dest=bay, chan_ident=channel))
+
+        if x == 1:
+            self.loopparams = self.loopparams_[0][0]
+            """Alias to first bay/channel of :data:`loopparams_`."""
+
+    def _process_message(self, m):
+        super()._process_message(m)
+
+        # Decode bay and channel IDs and check if they match one of ours
+        if m.msg in ("mot_get_kcubekstloopparams",):
+            # Check if source matches one of our bays
+            try:
+                bay_i = self.bays.index(m.source)
+            except ValueError:
+                # Ignore message from unknown bay id
+                self._log.warn(f"Message {m.msg} has unrecognised source={m.source}.")
+                bay_i = 0
+            # Check if channel matches one of our channels
+            try:
+                channel_i = self.channels.index(m.chan_ident)
+            except ValueError:
+                # Ignore message from unknown channel id
+                self._log.warn(f"Message {m.msg} has unrecognised channel={m.chan_ident}.")
+                channel_i = 0
+        
+        if m.msg == "mot_get_kcubekstloopparams":
+            # Control loop update message
+            self.loopparams_[bay_i][channel_i].update(m._asdict())
+
+
+    def set_loopparams(self, loop_mode=None, prop=None, integral=None, diff=None, pid_clip=None, pid_tol=None, encoder_const=None, bay=0, channel=0):
+        """
+        Configure the closed-loop positioning parameters used by encoded stages.
+
+        Note that the keyword parameter to modify the "int" value is named "integral" so as not to confuse it with the python int keyword.
+        """
+        if loop_mode is None:
+            loop_mode = self.loopparams_[bay][channel]["loop_mode"]
+        if prop is None:
+            prop = self.loopparams_[bay][channel]["prop"]
+        if integral is None:
+            integral = self.loopparams_[bay][channel]["int"]
+        if diff is None:
+            diff = self.loopparams_[bay][channel]["diff"]
+        if pid_clip is None:
+            pid_clip = self.loopparams_[bay][channel]["pid_clip"]
+        if pid_tol is None:
+            pid_tol = self.loopparams_[bay][channel]["pid_tol"]
+        if encoder_const is None:
+            encoder_const = self.loopparams_[bay][channel]["encoder_const"]
+
+        self._log.debug(f"Setting control loop parameters for [bay={self.bays[bay]:#x}, channel={self.channels[channel]}].")
+        self._loop.call_soon_threadsafe(self._write, apt.mot_set_kcubekstloopparams(source=EndPoint.HOST, dest=self.bays[bay], chan_ident=self.channels[channel], loop_mode=loop_mode, prop=prop, int=integral, diff=diff, pid_clip=pid_clip, pid_tol=pid_tol, encoder_const=encoder_const))
+        # Update status with new loop parameters
+        self._loop.call_soon_threadsafe(self._write, apt.mot_req_kcubekstloopparams(source=EndPoint.HOST, dest=self.bays[bay], chan_ident=self.channels[channel]))
+
+
+class BSC201(BSC):
+    """
+    A class for ThorLabs APT device model BSC201.
+
+    It is based off :class:`BSC`, but looking for a serial number starting with ``"40"`` and setting ``x = 1``.
 
     :param serial_port: Serial port device the device is connected to.
     :param vid: Numerical USB vendor ID to match.
@@ -49,7 +153,7 @@ class BSC201(APTDevice_BayUnit):
     """
     def __init__(self, serial_port=None, vid=None, pid=None, manufacturer=None, product=None, serial_number="40", location=None, home=True, invert_direction_logic=False, swap_limit_switches=True):
 
-        super().__init__(serial_port=serial_port, vid=vid, pid=pid, manufacturer=manufacturer, product=product, serial_number=serial_number, location=location, x=1, home=home, invert_direction_logic=invert_direction_logic, swap_limit_switches=swap_limit_switches, status_updates="polled")
+        super().__init__(serial_port=serial_port, vid=vid, pid=pid, manufacturer=manufacturer, product=product, serial_number=serial_number, location=location, x=1, home=home, invert_direction_logic=invert_direction_logic, swap_limit_switches=swap_limit_switches)
 
 
 class BSC201_DRV250(BSC201):
